1c0e199a4cdc5ece3e8d833fdfb059f69aafab57
evx001 <eviexeris@yahoo.com>
2013-12-18 15:22:15 -0500

    [1,2,fz...98,fz,bz]

--- fbzMixR.hs
+++ fbzMixR.hs
@@ -1,13 +1,7 @@
-module Dec18 where 
-import ListSort
--- import PrImEs
-find'   k t   = [v | (k', v) <- t, k == k']
+module FbzMixR where 
 swap' (x,y)   = (y,x)
 nList xs      = [if x `mod` 15 ==0  then "fbz" else if x `mod` 5 == 0 then "bz" else if x  `mod` 3 == 0 then "fz" else  "o" | x <- xs ]
 zzzs          = zip (nList (reverse[1..100])) (reverse[1..100])
-zzzsr         = zip (reverse[1..100]) (nList (reverse[1..100]))
-rzzzs         = reverse zzzs 
-rzzzsr        = reverse zzzsr
 firsts ps           = [x| (x,_)<- ps]
 seconds ps          = [y| (_,y)<-ps]
 strTups :: [([Char],[Char])] -- interpretor returns:  strTups :: [([Char], String)] changed it here to make (a,b) same typeclass as (b,a) 
