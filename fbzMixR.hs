7c92f98d7fb9fb478da584ab9683dba88ab16715
evx001 <eviexeris@yahoo.com>
2014-01-01 23:06:18 -0500

    added in myClojure examp

--- fbzMixR.hs
+++ fbzMixR.hs
@@ -63,7 +63,24 @@ Clojure
 		(range 1 101))]
 	(println x))
 --------------------------------------
+My own Clojure without doseq
 
+(ns fizzBuzz)
+
+(loop [x 100]
+  (when (> x 0)
+(cond 
+   (= 0 (mod x 15))
+  (print   "FizzBuzz ")
+     (= 0 (mod x 5))
+   (print   "Buzz ")
+      (= 0 (mod x 3))
+    (print   "Fizz ")
+        (<= 0 (mod x 2))
+      (print x\ ))
+  (recur (- x 1))))
+
+--------------------------------------
 
 let strTups = zip (firsts zzzs)  [show x|x <-(seconds zzzs)]
  
