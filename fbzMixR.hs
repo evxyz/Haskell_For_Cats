40defd82a540a57099772ec3189a580bff96417e
evx001 <eviexeris@yahoo.com>
2014-01-01 22:59:52 -0500

    better solutions

--- fbzMixR.hs
+++ fbzMixR.hs
@@ -17,13 +17,53 @@ test15	= [x `mod`15==0  |x <-nList]
 ["bz","fz","fz","bz","fz","fbz","fz","bz","fz","fz","bz","fz","fbz","fz","bz","fz","fz","bz","fz","fbz","fz","bz","fz","fz","bz","fz","fbz","fz","bz","fz","fz","bz","fz","fbz","fz","bz","fz","fz","bz","fz","fbz","fz","bz","fz","fz","bz","fz"]
 *Dec18> [x|x<-(firsts strTups),x == "o"]
 ["o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o"]
--}
+---------------------------------------
+ fizzbuzz xs = [if x `mod` 3 == 0 && x `mod` 5 == 0 then "FizzBuzz"
+		 else if x `mod` 3 == 0 then "Fizz"
+		 else if x `mod` 5 == 0 then "Buzz"
+		 else show x
+		 | x <- xs]
+---------------------------------------
+main = print [ fizzbuzz x | x <- [1..100] ]
+	where fizzbuzz x
+		| x `multipleOf` [3, 5] = "FizzBuzz"
+		| x `multipleOf` [3]	= "Fizz"
+		| x `multipleOf` [5]	= "Buzz" 
+		| otherwise		= show x
+		where m `multipleOf` ns = 
+			all (\n -> m `mod` n == 0) ns
+---------------------------------------
+Common Lisp using conditional formatting:
+  (loop for i from 1 to 100 do
+	(format t "~[Fizz~;~]~[Buzz~;~]~:[~a~;~] "
+	(mod i 3) (mod i 5) (zerop (* (mod i 3) (mod i 5))) i))
+---------------------------------------
+Scala!
+	(1 to 100) foreach { n => 
+		if (n % 3 == 0) print("Fizz")
+		if (n % 5 == 0) print("Buzz")
+		if (n % 3 != 0 && n % 5 != 0) print(n)
+		println
+	}
+---------------------------------------
+F#
+[1..100] |> Seq.map (function
+	| x when x%5=0 && x%3=0 -> "FizzBuzz"
+	| x when x%3=0 -> "Fizz"
+	| x when x%5=0 -> "Buzz"
+	| x -> string x)
+|> Seq.iter (printfn "%s")
 
+--------------------------------------
+Clojure
+ (doseq [x (map #(cond (zero? (mod % 15)) "FizzBuzz"
+			(zero? (mod % 3)) "Fizz"
+			(zero? (mod % 5)) "Buzz"
+			:else %)
+		(range 1 101))]
+	(println x))
+--------------------------------------
 
-
-
-
-{-
 
 let strTups = zip (firsts zzzs)  [show x|x <-(seconds zzzs)]
  
