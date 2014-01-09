5b5ce04d0b82663eac41a3d0ee2516aafd0e512a
evx001 <eviexeris@gmail.com>
2014-01-08 23:56:34 -0500

    Update hugs ready

--- ch4-ch5/2014-0108.-hugs5.hs
+++ ch4-ch5/2014-0108.-hugs5.hs
@@ -1,4 +1,9 @@
+module Hugs5 where
+import Language.Haskell.Extension
+-- 5.7.1 -- 
+sumTo100Sq = [sum[x^2] | x <- [1..100]]
 
+-- 5.7.2 -- 
 -- [True, True, True ] --
 -- l2ngth :: Num a => [t] -> a 
 -- l2ngth xs = sum[1|_<-xs]
@@ -6,8 +11,6 @@
 repliKate n x = [x|_ <- [1..n]] 
  
 {- 5.7.3 -- 
- 
-
 pyths 10
 [(3, 4, 5), (4, 3, 5), (6, 8, 10), (8, 6, 10)]
 -}
@@ -23,7 +26,7 @@ pYths n           = [(x,y,z)|   x <- [1..n],
  For example:
  > perfects 500 [6, 28, 496]
  -} 
--
+
 perfects n = [x|x <- [1..n], sum (init(factors x)) == x]
  {- 5.7.5 -- 
  Show how the single comprehension 
@@ -33,13 +36,13 @@ perfects n = [x|x <- [1..n], sum (init(factors x)) == x]
  -}
  
 --- 5.7.6. Redefine the function positions using the function find.
----
--{- 5.7.7. The scalar product of two lists of integers xs and ys of length n is given by the sum of the products of corresponding integers:
+
+{- 5.7.7. The scalar product of two lists of integers xs and ys of length n is given by the sum of the products of corresponding integers:
 -- concat [[(x,y) | y <- [4,5,6]]|x <- [1,2,3]]
 -- [(1,4),(1,5),(1,6),(2,4),(2,5),(2,6),(3,4),(3,5),(3,6)]
  
 -n-1
--- 5.7.6. Redefine the function positions using the function find.
+-- 5.7.6. Redefine the function positions using the function find.-}
 pos3tions x xs = find x (zip xs [0..n]) 
                    where n = length xs -1 
  
@@ -66,28 +69,26 @@ scalasrproduct xs ys = sum [x * y | (x,y) <- zip xs ys]
 --- import GHC.TypeLits
 --- import System.Random
 shift                       :: Int -> Char -> Char 
-shift   n c | isLower c     = int2low ((low2int c + n) `mod`26)
+shift  n c | isLower c     = int2low ((low2int c + n) `mod`26)
            | isUpper c     = int2upp ((upp2int c + n) `mod`26)
            | otherwise     = c 
 
 freqs                       :: String -> [Float] 
-freqs   xs                  = [percent (count x xs') n | x <- [`a`..`z`]]
+freqs   xs                  = [percent (count x xs') n | x <- ['a'..'z']]
                                where 
                                    xs' = map toLower xs 
                                    n   = letters xs 
 low2int                     :: Char -> Int 
-low2int c                   =  ord c -> ord `a` 
+low2int c                   =  ord c - ord 'a' 
 
 int2low                     :: Int -> Char 
-int2low                     = chr (ord `a` + n)
+int2low                     = chr (ord 'a' + n)
 
 upp2int                     :: Char -> Int 
-upp2int c                   =  ord c - ord `A`
+upp2int c                   =  ord c - ord 'A'
 
 int2upp                     :: Int -> Char
-int2upp n                   = chr (ord `A` + n) 
+int2upp n                   = chr (ord 'A' + n) 
  
 letters                     :: String -> Int 
-letters xs                  = length [x|x <- xs, isAlpha] 
-
-
+letters xs                  = length [x|x <- xs, isAlpha x] 
